a51f65d4de0fb8f4e56241943794adda546ac6df
Ldir-acer <lekks@yandex.ru>
2018-03-06 10:37:53 +0300

    Rotated screen

--- dioxmeter.ino
+++ dioxmeter.ino
@@ -7,18 +7,20 @@
 
 #include <Adafruit_GFX.h>    // Core graphics library
 
-//#define LCD_CS A3 // Chip Select goes to Analog 3
-//#define LCD_CD A2 // Command/Data goes to Analog 2
-//#define LCD_WR A1 // LCD Write goes to Analog 1
-//#define LCD_RD A0 // LCD Read goes to Analog 0
-//
-//#define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
-//#include <TftSpfd5408.h> // Hardware-specific library
+/*
+#define LCD_CS A3 // Chip Select goes to Analog 3
+#define LCD_CD A2 // Command/Data goes to Analog 2
+#define LCD_WR A1 // LCD Write goes to Analog 1
+#define LCD_RD A0 // LCD Read goes to Analog 0
+
+#define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
+#include <TftSpfd5408.h> // Hardware-specific library
+*/
 
 #include <MCUFRIEND_kbv.h>
 MCUFRIEND_kbv tft;
 #define TFT_ID 0x9341
-#define TFT_ROTATE 3
+#define TFT_ROTATE 1
 
 // Assign human-readable names to some common 16-bit color values:
 #define	BLACK   0x0000
