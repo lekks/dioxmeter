c931d951d83d48f030472b6cfe0e616fc3d48718
Alexey Dolinenko <lekks@yandex.ru>
2025-04-22 23:08:41 +0300

    resturcture sources

---
+++ dioxmeter/config.h
@@ -0,0 +1,23 @@
+//Copy to config.h and tune
+
+// Display properties
+#define TFT_ID 0x9341
+#define TFT_ROTATE 1 //USE 1 and 3 for horisontal orientation
+
+
+// Chart properties
+#define  MIN_CHART_VALUE 400
+#define  MAX_CHART_VALUE 2000
+
+#define MIN_CHART_COLOR 60
+#define MAX_CHART_COLOR 255
+
+#define DISPLAY_WIDTH_PX 320
+#define DISPLAY_HEIGHT_PX 240
+
+#define  BACKGROUND_COLOR BLACK
+
+
+// Sensor and measurement properties
+#define HEATING_DELAY 120000L
+#define MAX_PPM 5000
